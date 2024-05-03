7224bbee23c38c8e67c30ed8430f75de9b962afb
Mohamed Alabasy <eng.mohamed.alabasy@gmail.com>
2024-05-03 17:51:40 +0300

    update school from Shared-Service

--- src/school/school.module.ts
+++ src/school/school.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { SchoolService } from './school.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { SchoolController } from './school.controller';
+import { SCHOOL } from 'Shared-Service/src/entities-name';
+import { SchoolSchema } from 'Shared-Service/src/school/entities/school.entity';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: SCHOOL, schema: SchoolSchema }])],
   controllers: [SchoolController],
   providers: [SchoolService],
+  exports: [SchoolService],
 })
-export class SchoolModule {}
+
+export class SchoolModule { }
